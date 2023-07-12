e0504e7a5b78aff654008139d290e9949b0553d3
ge0rgeth0mas <georgethomas1912@gmail.com>
2023-07-12 22:54:42 +0530

    Building app continues

--- telco_customer_churn/web_app/churn_predictor_app.py
+++ telco_customer_churn/web_app/churn_predictor_app.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import pickle
 import json
 from pathlib import Path
 import pandas as pd
 import numpy as np
+from typing import Optional
+import uvicorn
 
 app = FastAPI()
 
@@ -33,10 +35,62 @@ try:
 except Exception as e:
     print("Error while loading columns: ", e)
 
-@app.get('/')
-def index():
-    return {'data':{'name':'George'}}
 
-@app.get('/about')
-def about():
-    return {'data':'about page'}
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory='templates')
+
+class CustomerInput(BaseModel):
+    
+    gender : str
+    SeniorCitizen : str
+    Partner : str
+    Dependents : str
+    tenure : int
+    MultipleLines : str = 'No'
+    InternetService : str
+    OnlineSecurity : str = 'No'
+    OnlineBackup : str = 'No'
+    DeviceProtection : str = 'No'
+    TechSupport : str = 'No'
+    StreamingTV : str = 'No'
+    StreamingMovies : str = 'No'
+    Contract : str
+    PaperlessBilling : str
+    PaymentMethod : str
+    MonthlyCharges : float
+
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post('/predict')
+async def predict_churn(customer_input: CustomerInput):
+    input_data = customer_input.model_dump()
+    input_data['TotalCharges']=input_data['tenure']*input_data['MonthlyCharges']
+    input_data = pd.DataFrame([input_data])
+    X = transformer.transform(input_data)
+    X = pd.DataFrame(X, columns=column_names)
+    print(X)
+    prediction = model.predict(X)
+    # return {"churn_prediction": bool(prediction[0])}
+    return X.to_dict()
+
+
+#   "Gender": "Male",
+#   "SeniorCitizen": "Yes",
+#   "Partner": "Yes",
+#   "Dependents": "Yes",
+#   "MultipleLines": "Yes",
+#   "InternetService": "DSL",
+#   "OnlineSecurity": "Yes",
+#   "OnlineBackup": "Yes",
+#   "DeviceProtection": "Yes",
+#   "TechSupport": "Yes",
+#   "StreamingTV": "Yes",
+#   "StreamingMovies": "Yes",
+#   "PaperlessBilling": "Yes",
+#   "PaymentMethod": "Mailed check",
+#   "Contract": "One year",
+#   "Tenure": 2,
+#   "MonthlyCharges": 30.00
